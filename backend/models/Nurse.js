5ebe7da93f9042522b153a6fa699dfa898b0d195
shobhit15082003 <shobhit.2201140ec@iiitbh.ac.in>
2024-12-16 18:10:25 +0530

    removal from models

--- backend/models/Nurse.js
+++ backend/models/Nurse.js
@@ -1,43 +1,43 @@
 import mongoose from "mongoose";    
 
 const nurseSchema  = new mongoose.Schema({
-    first_name:{
-        type:String,
-        required:true,
-        trim:true,
-    },
-    last_name:{
-        type:String,
-        required:true,
-        trim:true,
-    },
-	contact_info:{
-        type:String,
-        required:true,
-    },
-	gender:{
-        type:String,
-    },
-    license_number:{
-        type:String,
-		unique: true, 
-    },
-    work_address:{
-        type:String,
-        required:true,
-    },
-    image:{
-        type:String,
-    },
-    user_id:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
+    // first_name:{
+    //     type:String,
+    //     required:true,
+    //     trim:true,
+    // },
+    // last_name:{
+    //     type:String,
+    //     required:true,
+    //     trim:true,
+    // },
+	// contact_info:{
+    //     type:String,
+    //     required:true,
+    // },
+	// gender:{
+    //     type:String,
+    // },
+    // license_number:{
+    //     type:String,
+	// 	unique: true, 
+    // },
+    // work_address:{
+    //     type:String,
+    //     required:true,
+    // },
+    // image:{
+    //     type:String,
+    // },
+    // user_id:{
+    //     type:mongoose.Schema.Types.ObjectId,
+    //     ref:"User",
 
-    }, //sus
-    yearsOfExperience:{
-        type:String,
-        required:true,
-    },
+    // }, //sus
+    // yearsOfExperience:{
+    //     type:String,
+    //     required:true,
+    // },
     
 
 	
